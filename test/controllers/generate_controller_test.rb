d7cbf30fd240e80910b3d4792f861c14428fdba3
Ilgiz Mustafin <ilgimustafin@gmail.com>
2020-07-03 04:12:42 +0300

    Delete old tests

--- test/controllers/generate_controller_test.rb
+++
@@ -1,9 +0,0 @@
-require 'test_helper'
-
-class GenerateControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
-    get generate_index_url
-    assert_response :success
-  end
-
-end
