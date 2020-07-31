437b0d964e8f3aac8d62ee3a0ee119612184f224
Ilgiz Mustafin <ilgimustafin@gmail.com>
2020-07-31 21:43:00 +0300

    Add texts retrieval

--- app/services/vk_posts.rb
+++ app/services/vk_posts.rb
@@ -1,12 +1,26 @@
 class VkPosts
   def initialize
-    @app_id = Rails.configuration.vk_app_id
-    @secret = Rails.configuration.vk_secret
-    @service = Rails.configuration.vk_service
+    @app_id = Rails.configuration.x.vk_app_id
+    @secret = Rails.configuration.x.vk_secret
+    @service = Rails.configuration.x.vk_service
   end
 
-  def posts(domain)
+  def request(method, params)
+    path = "https://api.vk.com/method/#{method}"
 
+    configs = {
+      access_token: @service,
+      v: '5.122'
+    }
+    response = HTTParty.get(path, query: params.merge(configs))
+    JSON.parse(response.body)['response']
+  end
 
+  def wall_get_many(domain)
+    request('wall.get', domain: domain, count: 100)['items']
+  end
+
+  def texts(domain)
+    wall_get_many(domain).map { |p| p['text'] }
   end
 end
