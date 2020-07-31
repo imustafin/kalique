80d252ee9b5a7b95f88103464643b96a7cbe328a
Ilgiz Mustafin <ilgimustafin@gmail.com>
2020-08-01 00:08:24 +0300

    Add admin page loading/deleting

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
   post 'generate/index'
 
   get 'admin', to: 'admin#index', as: 'admin'
+  post 'admin_load_posts', to: 'admin#load_posts'
+  post 'admin_delete_all_posts', to: 'admin#delete_all_posts'
 
   root 'generate#index'
 end
