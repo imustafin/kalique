83d3516423621bf8064c40dadacb2b4069eea44a
Ilgiz Mustafin <ilgimustafin@gmail.com>
2020-10-08 22:43:01 +0300

    Add basic chain algo

--- app/services/text_generator.rb
+++ app/services/text_generator.rb
@@ -2,40 +2,87 @@ class TextGenerator
   def generate_with_strings(seed, strings)
     return nil unless strings && !strings.empty?
 
-    prefix = seed || ''
-
-    markov = MarkyMarkov::TemporaryDictionary.new(2)
-    strings.each do |t|
-      markov.parse_string(pack_string(t.to_s))
-    end
-
-    prefix + ' ' + unpack_string(markov.generate_10_sentences)
   end
 
   def generate(seed)
-    generate_with_strings(seed, SourceText.pluck(:text))
+    refresh_tree
+
+    gen_internal(seed)
   end
 
+  private
 
-  PACK_MAP = {
-    "\n" => "\1"
-  }
-  def pack_string(s)
-    ans = s
-    PACK_MAP.each do |k, v|
-      ans = ans.gsub(k, v)
+  DEPTH = 10
+
+  def refresh_tree
+    posts = SourceText.pluck(:text)
+
+    @root = Node.new
+
+    posts.each do |post|
+      post.chars.each_cons(DEPTH) do |tok|
+        v = @root
+        tok.each { |t| v = v.add(t) }
+      end
+    end
+  end
+
+  def gen_internal(seed)
+    ans = seed || @root.get.first
+
+    100.times do
+      last = ans.last(DEPTH - 1).chars
+
+      pp last
+
+      v = @root
+      path = []
+      until last.empty?
+        char = last.shift
+        v = v.step(char)
+
+        unless v
+          v = @root.step(char)
+          path = []
+        end
+
+        path << char
+
+        pp path
+      end
+
+      ans += v.get.first || @root.get.first
     end
 
     ans
   end
 
-  def unpack_string(s)
-    ans = s
-
-    PACK_MAP.each do |k, v|
-      ans = ans.gsub(v, k)
+  class Node
+    def initialize
+      @children = Hash.new { [0, Node.new] } # char => [count, Node]
     end
 
-    ans
+    def add(char)
+      raise "Can add only one char (#{char} given)" if char.length != 1
+
+      child = @children[char]
+      child[0] += 1
+
+      @children[char] = child
+
+      child.last
+    end
+
+    def get
+      sum_counts = @children.values.map(&:first).sum
+
+      char, (_, node) = @children.max_by { |_, (count, _)| rand ** (1. / (count.to_f / sum_counts)) }
+
+      [char, node]
+    end
+
+    def step(char)
+      @children[char].last
+    end
   end
 end
