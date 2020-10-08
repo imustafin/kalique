c3215ba586d194dcf11c9acc187d4e9d1dc451bf
Ilgiz Mustafin <ilgimustafin@gmail.com>
2020-10-08 23:54:17 +0300

    Fix tests

--- app/services/text_generator.rb
+++ app/services/text_generator.rb
@@ -2,10 +2,13 @@ class TextGenerator
   def generate_with_strings(seed, strings)
     return nil unless strings && !strings.empty?
 
+    refresh_tree(strings)
+
+    gen_internal(seed)
   end
 
   def generate(seed)
-    refresh_tree
+    refresh_tree(get_texts)
 
     gen_internal(seed)
   end
@@ -30,10 +33,10 @@ class TextGenerator
     all_posts.map(&:downcase)
   end
 
-  def refresh_tree
+  def refresh_tree(strings)
     @root = Node.new
 
-    get_texts.each do |post|
+    strings.each do |post|
       post = BEFORE_FIRST + post + AFTER_LAST
 
       post.chars.each_cons(DEPTH) do |tok|
@@ -44,7 +47,9 @@ class TextGenerator
   end
 
   def gen_internal(seed)
-    ans = seed.empty? ? BEFORE_FIRST : seed
+    return nil if @root.empty?
+
+    ans = (!seed || seed.empty?) ? BEFORE_FIRST : seed
 
     loop do
       last = ans.last(DEPTH - 1).chars
@@ -76,6 +81,10 @@ class TextGenerator
   class Node
     def initialize
       @children = Hash.new { [0, Node.new] } # char => [count, Node]
+    end
+
+    def empty?
+      @children.empty?
     end
 
     def add(char)
